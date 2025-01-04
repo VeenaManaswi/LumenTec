4ddc18bcc2edeab36d641b93c322a1ca1db64aac
VeenaManaswi <kvmanaswi@gmail.com>
2025-01-04 15:19:42 +0530

    Newcommit

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login2';
 import Signup from './components/Signup2';
-import UserProfile from './components/UserProfile';
-import FilterCard from './components/FilterCard';
-import './App.css';
+import invman from './images/invman.png';
+import './App.css';  // Import App.css for styling
+import UserProfile from './components/UserProfile'
+import FilterCard from './components/FilterCard'
 
 function App() {
-  const [showLogin, setShowLogin] = useState(true);
+  const [showLogin, setShowLogin] = useState(true); // Toggle between Login and Signup
 
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          {/* Authentication Page (Login/Signup) */}
-          <Route
-            path="/"
-            element={
-              <div className="box">
-                <div className="form-container">
-                  {showLogin ? (
-                    <Login />
-                  ) : (
-                    <Signup />
-                  )}
-                  <div className="toggle-container">
-                    <button onClick={() => setShowLogin(true)} disabled={showLogin}>
-                      Login
-                    </button>
-                    <button onClick={() => setShowLogin(false)} disabled={!showLogin}>
-                      Signup
-                    </button>
-                  </div>
-                </div>
-              </div>
-            }
-          />
-          
-          {/* User Profile Page */}
-          <Route path="/user-profile" element={<UserProfile />} />
-          
-          {/* Filter Card Page */}
-          <Route path="/filter-card" element={<FilterCard />} />
-        </Routes>
+    <>
+    <br />
+    <center><h1> Telecom Inventory Management System</h1></center>
+    <div className="app-container">
+      <div className="box">
+        <div className="form-container">
+          {showLogin ? (
+            <Login />
+          ) : (
+            <Signup />
+          )}
+          <div className="toggle-container">
+            <button
+              onClick={() => setShowLogin(true)}
+              disabled={showLogin}
+            >
+              Login
+            </button>
+            <button
+              onClick={() => setShowLogin(false)}
+              disabled={!showLogin}
+            >
+              Signup
+            </button>
+          </div>
+        </div>
       </div>
-    </Router>
+    </div>
+    <br /><br />
+    <UserProfile/>
+    <br /><br />
+    <FilterCard/>
+    </>
+
   );
 }
 
 export default App;
+
